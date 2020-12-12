3eb2065abcec0fe2c51f407bf828a2facd75fe01
David <dmaster18@gmail.com>
2020-12-12 10:10:29 -0500

    Reseeded rails db

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_014908) do
+ActiveRecord::Schema.define(version: 2020_12_12_001313) do
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "score"
+    t.integer "rank"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "words", force: :cascade do |t|
     t.string "name"
