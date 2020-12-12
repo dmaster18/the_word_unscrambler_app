fadeb46f5768cf0f11e18eac17added69d7347e7
David <dmaster18@gmail.com>
2020-12-12 13:00:12 -0500

    Modified handleTileClick

--- frontend/src/game-components/Word.js
+++ frontend/src/game-components/Word.js
@@ -4,14 +4,22 @@ import React, { Component } from 'react';
 export default class Word extends Component {
   constructor(props) {
     super(props)
-    /*this.state = {
+    this.word = []
+    this.state = {
       wordFormed: '',
       wordsFormed: []
-    };*/
+    }
   }
 
   tilesCreator() {
-    return (this.props.randomLetters).map(letter => <div className='tile'>{letter}</div>)
+    return (this.props.randomLetters).map(letter => <div className='tile'><button onClick={this.handleTileClick} value={letter}>{letter}</button></div>)
+  }
+
+  handleTileClick = (event) => {
+    this.word.push(event.target.value)
+    this.setState({
+      wordFormed: this.word.join('')
+    })
   }
 
   render() {
@@ -22,12 +30,3 @@ export default class Word extends Component {
     )
   }
 }
-
-/*<button onClick={this.handleTileClick}>*/
-/*</button>*/
-  /*handleTileClick() {
-    this.word.push(this.innerText)
-    this.setState({
-      wordFormed: this.word.join('')
-    })
-  }*/
