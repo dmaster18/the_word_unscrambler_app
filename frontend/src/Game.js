da476327c26c32be5bc895e2403190838bf6fc73
David <dmaster18@gmail.com>
2020-12-15 21:10:21 -0500

    Another Game.js update

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -43,7 +43,7 @@ export default class Game extends Component {
   }
 
   //generateWord() {
-    //<WordContainer name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words} nineLetterWords={this.state.gameInfo.nine_letter_words} />
+    //<WordContainer name={this.state.wordSession.name} twoLetterWords={this.state.wordSession.two_letter_words} threeLetterWords={this.state.wordSession.three_letter_words} fourLetterWords={this.state.wordSession.four_letter_words} fiveLetterWords={this.state.wordSession.five_letter_words} sixLetterWords={this.state.wordSession.six_letter_words} sevenLetterWords={this.state.wordSession.seven_letter_words} eightLetterWords={this.state.wordSession.eight_letter_words} nineLetterWords={this.state.wordSession.nine_letter_words} />
     //json.data.map(word => {
     //  <WordContainer name={word.name} twoLetterWords={word.two_letter_words} threeLetterWords={word.three_letter_words} fourLetterWords={word.four_letter_words} fiveLetterWords={word.five_letter_words} sixLetterWords={word.six_letter_words} sevenLetterWords={word.seven_letter_words} eightLetterWords={word.eight_letter_words} nineLetterWords={word.nine_letter_words} />
     //})
@@ -55,15 +55,15 @@ export default class Game extends Component {
   render() {
     return (
       <div id="game">
-        <WordContainer letterArray={this.shuffle(this.state.gameInfo.name)} name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words}
+        <WordContainer letterArray={this.shuffle(this.state.wordSession.name)} name={this.state.wordSession.name} allWords={this.state.wordSession.all_words} />
       </div>
     )
   }
 }
 
-/*  {this.state.gameInfo.name &&
+/*  {this.state.wordSession.name &&
     <>
-      <WordContainer letterArray={this.shuffle(this.state.gameInfo.name)} name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words} nineLetterWords={this.state.gameInfo.nine_letter_words} allWords={this.state.gameInfo.all_words}/>
+      <WordContainer letterArray={this.shuffle(this.state.wordSession.name)} name={this.state.wordSession.name} twoLetterWords={this.state.wordSession.two_letter_words} threeLetterWords={this.state.wordSession.three_letter_words} fourLetterWords={this.state.wordSession.four_letter_words} fiveLetterWords={this.state.wordSession.five_letter_words} sixLetterWords={this.state.wordSession.six_letter_words} sevenLetterWords={this.state.wordSession.seven_letter_words} eightLetterWords={this.state.wordSession.eight_letter_words} nineLetterWords={this.state.wordSession.nine_letter_words} allWords={this.state.wordSession.all_words}/>
     </>
   }*/
 
