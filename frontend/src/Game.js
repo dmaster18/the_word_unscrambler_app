048b141d1159c8742279310c94b9971a5e00ca95
David <dmaster18@gmail.com>
2020-12-15 21:08:29 -0500

    Update

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -17,7 +17,7 @@ export default class Game extends Component {
   }
 
   fetchWords() {
-    const words_url = 'http://127.0.0.1:3000/words';
+    const words_url = 'http://127.0.0.1:3000/words/limit?=10';
     return fetch(words_url).then(resp => resp.json());
   }
 
@@ -29,16 +29,18 @@ export default class Game extends Component {
 /*    this.i+=1;*/
 
   renderWord(words) {
-    return words.data[this.i].attributes.name
+    return console.log(words.data[this.i].attributes.name)
   }
 
 
-  /*componentDidMount() {
-    this.fetchWords().then(json => {
+  componentDidMount() {
+    this.fetchWords().then(words => {
     this.setState(
-      {gameInfo: json.data.attributes}
+      {gameInfo: words.data,
+      wordSession: words.data[this.i].attributes
+      }
     )})
-  }*/
+  }
 
   //generateWord() {
     //<WordContainer name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words} nineLetterWords={this.state.gameInfo.nine_letter_words} />
@@ -53,7 +55,7 @@ export default class Game extends Component {
   render() {
     return (
       <div id="game">
-        {this.initiateGame()}
+        <WordContainer letterArray={this.shuffle(this.state.gameInfo.name)} name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words}
       </div>
     )
   }
