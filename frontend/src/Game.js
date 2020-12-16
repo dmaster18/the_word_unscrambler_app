2c641ebba6b407e2c1ced8b91c350ed19cf9a093
David <dmaster18@gmail.com>
2020-12-15 19:42:26 -0500

    Updated Game.js

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -33,12 +33,12 @@ export default class Game extends Component {
   }
 
 
-  componentDidMount() {
+  /*componentDidMount() {
     this.fetchWords().then(json => {
     this.setState(
       {gameInfo: json.data.attributes}
     )})
-  }
+  }*/
 
   //generateWord() {
     //<WordContainer name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words} nineLetterWords={this.state.gameInfo.nine_letter_words} />
@@ -53,18 +53,17 @@ export default class Game extends Component {
   render() {
     return (
       <div id="game">
-      {this.state.gameInfo.name &&
-        <>
-          <WordContainer letterArray={this.shuffle(this.state.gameInfo.name)} name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words} nineLetterWords={this.state.gameInfo.nine_letter_words} allWords={this.state.gameInfo.all_words}/>
-        </>
-      }
-
+        {this.initiateGame();}
       </div>
     )
   }
 }
 
-
+/*  {this.state.gameInfo.name &&
+    <>
+      <WordContainer letterArray={this.shuffle(this.state.gameInfo.name)} name={this.state.gameInfo.name} twoLetterWords={this.state.gameInfo.two_letter_words} threeLetterWords={this.state.gameInfo.three_letter_words} fourLetterWords={this.state.gameInfo.four_letter_words} fiveLetterWords={this.state.gameInfo.five_letter_words} sixLetterWords={this.state.gameInfo.six_letter_words} sevenLetterWords={this.state.gameInfo.seven_letter_words} eightLetterWords={this.state.gameInfo.eight_letter_words} nineLetterWords={this.state.gameInfo.nine_letter_words} allWords={this.state.gameInfo.all_words}/>
+    </>
+  }*/
 
 
           /*<Timer/>*/
