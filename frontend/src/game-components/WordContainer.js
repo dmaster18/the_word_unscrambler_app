d3cd5fe76d27f52d76c9dfa27441a35f20c772a4
David <dmaster18@gmail.com>
2020-12-16 21:00:42 -0500

    Updated value of this.score in WordContainer.js

--- frontend/src/game-components/WordContainer.js
+++ frontend/src/game-components/WordContainer.js
@@ -28,7 +28,7 @@ export default class WordContainer extends Component {
 
   render() {
     return (
-      <Word letterArray={this.props.letterArray} evaluateWord={this.evaluateWord} correctWords={this.state.correctWords} incorrectWords={this.state.incorrectWords} score={this.state.score}/>
+      <Word letterArray={this.props.letterArray} evaluateWord={this.evaluateWord} correctWords={this.state.correctWords} incorrectWords={this.state.incorrectWords} score={this.props.score}/>
     )
   }
 }
