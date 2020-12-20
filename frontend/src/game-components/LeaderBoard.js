d02ec9c95cc7944b1e2fcacf20b43a6ac4fac5e9
David <dmaster18@gmail.com>
2020-12-19 20:34:55 -0500

    Updated Leaderboard.js again

--- frontend/src/game-components/LeaderBoard.js
+++ frontend/src/game-components/LeaderBoard.js
@@ -1,44 +1,34 @@
 import React, { Component } from 'react';
 
 export default class Leaderboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerData: []
+    }
+  }
 
   fetchplayers () {
     const playersURL = 'http://127.0.0.1:3000/players';
-    return fetch(playersURL).then(resp => resp.json());
+    const playerData = fetch(playersURL).then(resp => resp.json());
+    this.setState(
+      {playerData: playerData}
+    )
   }
 
-  renderLoadingState () {
-    const main = document.querySelector('main');
-    main.innerHTML = '';
-    const gifContainer = document.createElement('div');
-    gifContainer.classList.add('gif-container');
-    const gifElement = document.createElement('img');
-    gifElement.src = '../gifs/old_bartender.gif';
-    gifContainer.appendChild(gifElement);
-    main.appendChild(gifContainer);
-  }
-
-  renderLeaderboard (json) {
-    const main = document.querySelector('main');
-    const playerData = json.data;
-    const leaderboard = document.createElement('table');
-    leaderboard.classList.add('leaderboard');
-    const leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>';
-    const arrayOfPlayerData = playerData.map(player => `<tr><td>${player.attributes.name}</td> <td>${player.attributes.score} Points</td> <td>${player.attributes.percentage}%</td></tr>`);
+  renderLeaderboard () {
+    const leaderboardHeader = '<tr><th>Name</th><th>Score</th>/tr>';
+    const arrayOfPlayerData = this.state.playerData.map(player => `<tr><td>${player.attributes.name}</td> <td>${player.attributes.score} Points</td></tr>`);
     const leaderboardData = arrayOfPlayerData.join(' ');
     leaderboard.innerHTML = `${leaderboardHeader} ${leaderboardData}`;
     main.appendChild(leaderboard);
   }
 
   render () {
-    const main = document.querySelector('main');
-    this.renderLoadingState();
-    main.innerHTML = '';
-    this.fetchplayers().then(json => this.renderLeaderboard(json));
+    return (<table className='leaderboard'>
+      {this.renderLeaderboard()}
+    </table>
+  })
   }
-}
-
-  render() {
-    return null
-  }
+  
 }
