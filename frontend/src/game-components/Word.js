9a4ed3c58900bc2ad6b3006e5bd0ef4f0f8eb3b1
David <dmaster18@gmail.com>
2020-12-20 16:38:06 -0500

    Moved state values from Word.js into WordContainer.js

--- frontend/src/game-components/Word.js
+++ frontend/src/game-components/Word.js
@@ -4,67 +4,10 @@ import React, { Component } from 'react';
 export default class Word extends Component {
   constructor(props) {
     super(props)
-    this.eventIDArray = []
-    this.wordArray = []
-    this.words = []
-    this.state = {
-      warning: null,
-      wordFormed: '',
-      wordsSubmitted: []
-    }
   }
 
   tilesCreator() {
-    return (this.props.letterArray).map((letter, index) => <button onClick={this.handleTileClick} value={letter} className='tile'id={index} key={`${letter}${index}`}>{letter}</button>)
-  }
-
-  handleTileClick = (event) => {
-      this.setState({
-          warning: null
-        });
-      if (!this.eventIDArray.includes(event.target.id)){
-        this.eventIDArray.push(event.target.id);
-        this.wordArray.push(event.target.value);
-        this.setState({
-          wordFormed: this.wordArray.join('')
-        });
-      }
-      else {
-        this.setState({
-            warning: <div>CAN'T USE SAME TILE TWICE IN ONE WORD!</div>
-          })
-        return this.state.warning;
-      }
-  }
-
-  handleDeleteLetterButtonClick = () => {
-    let currentWord = document.getElementById('current-word').innerText;
-    currentWord = currentWord.slice(0,-1);
-    this.wordArray = currentWord.split('');
-    this.eventIDArray.pop();
-    this.setState(
-      {wordFormed: this.wordArray.join(''), warning: null}
-    )
-  }
-
-  handleDeleteWordButtonClick = () => {
-    let currentWord = document.getElementById('current-word').innerText;
-    currentWord = [];
-    this.wordArray = currentWord;
-    this.eventIDArray = [];
-    this.setState(
-      {wordFormed: this.wordArray.join(''), warning: null}
-    )
-  }
-
-  handleSubmitButtonClick = () => {
-    this.words.push(this.state.wordFormed.toUpperCase());
-    this.props.evaluateWord(this.state.wordFormed.toUpperCase());
-    this.wordArray = [];
-    this.eventIDArray = [];
-    this.setState(
-      {wordsSubmitted: this.words, warning: null}
-    )
+    return (this.props.letterArray).map((letter, index) => <button onClick={this.props.onTileClick} value={letter} className='tile'id={index} key={`${letter}${index}`}>{letter}</button>)
   }
 
   displaySubmittedWords = (words) => {
@@ -74,25 +17,25 @@ export default class Word extends Component {
   render() {
     return (
       <div>
-        <div id='display-current-word'> <span>Your Current Word: </span><div id='current-word' value={this.state.wordFormed}>{this.state.wordFormed}</div> </div>
+        <div id='display-current-word'> <span>Your Current Word: </span><div id='current-word' value={this.props.wordFormed}>{this.props.wordFormed}</div> </div>
         <div className='score'>
           Your Current Score: {this.props.score} Points
         </div>
         <div className='warning'>
-          {this.state.warning}
+          {this.props.warning}
         </div>
         <div className='tile-container'>
           {this.tilesCreator()}
         </div>
         <div className='buttons'>
-          <button onClick={this.handleDeleteLetterButtonClick}>Delete Last Letter</button>
-          <button onClick={this.handleDeleteWordButtonClick}>Delete Current Word</button>
-          <button onClick={this.handleSubmitButtonClick}>Submit Word</button>
+          <button onClick={this.props.onDeleteLetter}>Delete Last Letter</button>
+          <button onClick={this.props.onDeleteWord}>Delete Current Word</button>
+          <button onClick={this.props.onSubmit}>Submit Word</button>
         </div>
         <div className='submitted-words'>
           <div className='all-submitted-words'>
             <b><u>All Submitted Words:</u></b>
-            {this.displaySubmittedWords(this.state.wordsSubmitted)}
+            {this.displaySubmittedWords(this.props.submittedWords)}
           </div>
           <div className='correct-words'>
             <b><u>Correct Words</u></b>
--- frontend/src/game-components/WordContainer.js
+++ frontend/src/game-components/WordContainer.js
@@ -5,10 +5,15 @@ export default class WordContainer extends Component {
 
   constructor(props) {
     super(props)
+    this.eventIDArray = []
+    this.wordArray = []
+    this.words = []
     this.state = {
       score: this.props.score,
       correctWords: [],
-      incorrectWords: []
+      incorrectWords: [],
+      submittedWords: [],
+      warning: null,
     };
   }
 
@@ -16,19 +21,73 @@ export default class WordContainer extends Component {
     userInput = userInput.toUpperCase()
     if ((this.props.allWords).includes(userInput) && !this.state.correctWords.includes(userInput)){
       this.setState({
+       submittedWords: [...this.state.submittedWords, userInput],
        correctWords: [...this.state.correctWords, userInput]
       })
       this.props.increment(userInput);
     } else {
         this.setState({
+          submittedWords: [...this.state.submittedWords, userInput],
           incorrectWords: [...this.state.incorrectWords, userInput]
       })
     }
   }
 
+  handleTileClick = (event) => {
+      this.setState({
+          warning: null
+        });
+      if (!this.eventIDArray.includes(event.target.id)){
+        this.eventIDArray.push(event.target.id);
+        this.wordArray.push(event.target.value);
+        this.setState({
+          wordFormed: this.wordArray.join('')
+        });
+      }
+      else {
+        this.setState({
+            warning: <div>CAN'T USE SAME TILE TWICE IN ONE WORD!</div>
+          })
+        return this.props.warning;
+      }
+  }
+
+  handleDeleteLetterButtonClick = () => {
+    let currentWord = document.getElementById('current-word').innerText;
+    currentWord = currentWord.slice(0,-1);
+    this.wordArray = currentWord.split('');
+    this.eventIDArray.pop();
+    this.setState(
+      {wordFormed: this.wordArray.join(''), warning: null}
+    )
+  }
+
+  handleDeleteWordButtonClick = () => {
+    let currentWord = document.getElementById('current-word').innerText;
+    currentWord = [];
+    this.wordArray = currentWord;
+    this.eventIDArray = [];
+    this.setState(
+      {wordFormed: this.wordArray.join(''), warning: null}
+    )
+  }
+
+  handleSubmitButtonClick = () => {
+    this.words.push(this.state.wordFormed.toUpperCase());
+    this.props.evaluateWord(this.state.wordFormed.toUpperCase());
+    this.wordArray = [];
+    this.eventIDArray = [];
+    this.setState(
+      {wordsSubmitted: this.words, warning: null}
+    )
+  }
+
   render() {
     return (
-      <Word letterArray={this.props.letterArray} evaluateWord={this.evaluateWord} correctWords={this.state.correctWords} incorrectWords={this.state.incorrectWords} score={this.props.score}/>
+      <Word letterArray={this.props.letterArray} evaluateWord={this.evaluateWord}
+      correctWords={this.state.correctWords} incorrectWords={this.state.incorrectWords}
+      submittedWords={this.state.submittedWords} score={this.props.score}
+      warning={this.state.warning} onTileClick={this.handleTileClick} onDeleteLetter={this.handleDeleteLetterButtonClick} onDeleteWord={this.handleDeleteWordButtonClick} onSubmit={this.handleSubmitButtonClick}/>
     )
   }
 }
