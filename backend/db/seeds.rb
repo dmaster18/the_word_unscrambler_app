6328fe5e1b985ab758f869124bed7ab4848be1ea
David <dmaster18@gmail.com>
2020-12-29 20:32:53 -0500

    Update seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -3721,9 +3721,11 @@ nikolai = Player.new(name: 'Nikolai', score: 50)
 horatio = Player.new(name: 'Horatio', score: 75)
 esteban = Player.new(name: 'Esteban', score: 550)
 claudia = Player.new(name: 'Claudia', score: 10)
+pablo = Player.new(name: 'Pablo, score: 40')
 david.save
 raul.save
 nikolai.save
 horatio.save
 esteban.save
 claudia.save
+pablo.save
