e2886bdfdf2f94bbfaca57bffec4bed106fb5b9c
David <dmaster18@gmail.com>
2021-01-01 17:08:19 -0500

    Renamed **_word_collection with **_letter_word_collection

--- backend/app/models/word_collection.rb
+++ backend/app/models/word_collection.rb
@@ -1,37 +1,37 @@
 class WordCollection < ApplicationRecord
 
-#Ensures generate_word_collections runs before instance of it is saved
-  before_save :generate_word_collections
+#Ensures generate_letter_word_collections runs before instance of it is saved
+  before_save :generate_letter_word_collections
 
-  serialize :two_word_collection, Array
-  serialize :three_word_collection, Array
-  serialize :four_word_collection, Array
-  serialize :five_word_collection, Array
-  serialize :six_word_collection, Array
-  serialize :seven_word_collection, Array
-  serialize :eight_word_collection, Array
-  serialize :nine_word_collection, Array
+  serialize :two_letter_word_collection, Array
+  serialize :three_letter_word_collection, Array
+  serialize :four_letter_word_collection, Array
+  serialize :five_letter_word_collection, Array
+  serialize :six_letter_word_collection, Array
+  serialize :seven_letter_word_collection, Array
+  serialize :eight_letter_word_collection, Array
+  serialize :nine_letter_word_collection, Array
 
 #Adds all two- through nine-letter words to collections from all letter groupings
-  def generate_word_collections
+  def generate_letter_word_collections
     Word.all.each {|word|
-      self.two_word_collection << word.two_letter_words
-      self.three_word_collection << word.three_letter_words
-      self.four_word_collection << word.four_letter_words
-      self.five_word_collection << word.five_letter_words
-      self.six_word_collection << word.six_letter_words
-      self.seven_word_collection << word.seven_letter_words
-      self.eight_word_collection << word.eight_letter_words
-      self.nine_word_collection << word.nine_letter_words
+      self.two_letter_word_collection << word.two_letter_words
+      self.three_letter_word_collection << word.three_letter_words
+      self.four_letter_word_collection << word.four_letter_words
+      self.five_letter_word_collection << word.five_letter_words
+      self.six_letter_word_collection << word.six_letter_words
+      self.seven_letter_word_collection << word.seven_letter_words
+      self.eight_letter_word_collection << word.eight_letter_words
+      self.nine_letter_word_collection << word.nine_letter_words
     }
-    self.two_word_collection = self.two_word_collection.uniq.flatten.sort
-    self.three_word_collection = self.three_word_collection.uniq.flatten.sort
-    self.four_word_collection = self.four_word_collection.uniq.flatten.sort
-    self.five_word_collection = self.five_word_collection.uniq.flatten.sort
-    self.six_word_collection = self.six_word_collection.uniq.flatten.sort
-    self.seven_word_collection = self.seven_word_collection.uniq.flatten.sort
-    self.eight_word_collection = self.eight_word_collection.uniq.flatten.sort
-    self.nine_word_collection = self.nine_word_collection.uniq.flatten.sort
+    self.two_letter_word_collection = self.two_letter_word_collection.uniq.flatten.sort
+    self.three_letter_word_collection = self.three_letter_word_collection.uniq.flatten.sort
+    self.four_letter_word_collection = self.four_letter_word_collection.uniq.flatten.sort
+    self.five_letter_word_collection = self.five_letter_word_collection.uniq.flatten.sort
+    self.six_letter_word_collection = self.six_letter_word_collection.uniq.flatten.sort
+    self.seven_letter_word_collection = self.seven_letter_word_collection.uniq.flatten.sort
+    self.eight_letter_word_collection = self.eight_letter_word_collection.uniq.flatten.sort
+    self.nine_letter_word_collection = self.nine_letter_word_collection.uniq.flatten.sort
     self
   end
 
