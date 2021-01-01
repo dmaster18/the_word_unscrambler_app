7cb2ae1b2e050793f98b06373d432f572347e3f7
David <dmaster18@gmail.com>
2021-01-01 13:14:14 -0500

    Add_all_words update

--- backend/app/models/word.rb
+++ backend/app/models/word.rb
@@ -17,26 +17,38 @@ class Word < ApplicationRecord
     capitalized_word_array
   end
 
-  def add_all_words
-    self.class.all_two_letter_words << self.two_letter_words
-    self.class.all_two_letter_words = self.class.all_two_letter_words.uniq
-    self.class.all_three_letter_words << self.three_letter_words
-    self.class.all_three_letter_words = self.class.all_three_letter_words.uniq
-    self.class.all_four_letter_words << self.four_letter_words
-    self.class.all_four_letter_words = self.class.all_four_letter_words.uniq
-    self.class.all_five_letter_words << self.five_letter_words
-    self.class.all_five_letter_words = self.class.all_five_letter_words.uniq
-    self.class.all_six_letter_words << self.six_letter_words
-    self.class.all_six_letter_words = self.class.all_six_letter_words.uniq
-    self.class.all_seven_letter_words << self.seven_letter_words
-    self.class.all_seven_letter_words = self.class.all_seven_letter_words.uniq
-    self.class.all_eight_letter_words << self.eight_letter_words
-    self.class.all_eight_letter_words = self.class.all_eight_letter_words.uniq
-    self.class.all_nine_letter_words << self.nine_letter_words
-    self.class.all_nine_letter_words = self.class.all_nine_letter_words.uniq
+  def aggregate_and_unique_all_words(all_word_array_type, word_array_type)
+    self.class.all_word_array_type << self.word_array_type
+    self.class.all_word_array_type = self.class.all_word_array_type.uniq
   end
 
-  def
+  def add_all_words(all_word_array_type, word_array_type)
+    aggregate_and_unique_all_words(all_two_letter_words, two_letter_words)
+    aggregate_and_unique_all_words(all_three_letter_words, three_letter_words)
+    aggregate_and_unique_all_words(all_four_letter_words, four_letter_words)
+    aggregate_and_unique_all_words(all_five_letter_words, five_letter_words)
+    aggregate_and_unique_all_words(all_six_letter_words, six_letter_words)
+    aggregate_and_unique_all_words(all_seven_letter_words, seven_letter_words)
+    aggregate_and_unique_all_words(all_eight_letter_words, eight_letter_words)
+    aggregate_and_unique_all_words(all_nine_letter_words, nine_letter_words)
+    #self.class.all_two_letter_words << self.two_letter_words
+    #self.class.all_two_letter_words = self.class.all_two_letter_words.uniq
+    #self.class.all_three_letter_words << self.three_letter_words
+    #self.class.all_three_letter_words = self.class.all_three_letter_words.uniq
+    #self.class.all_four_letter_words << self.four_letter_words
+    #self.class.all_four_letter_words = self.class.all_four_letter_words.uniq
+    #self.class.all_five_letter_words << self.five_letter_words
+    #self.class.all_five_letter_words = self.class.all_five_letter_words.uniq
+    #self.class.all_six_letter_words << self.six_letter_words
+    #self.class.all_six_letter_words = self.class.all_six_letter_words.uniq
+    #self.class.all_seven_letter_words << self.seven_letter_words
+    #self.class.all_seven_letter_words = self.class.all_seven_letter_words.uniq
+    #self.class.all_eight_letter_words << self.eight_letter_words
+    #self.class.all_eight_letter_words = self.class.all_eight_letter_words.uniq
+    #self.class.all_nine_letter_words << self.nine_letter_words
+    #self.class.all_nine_letter_words = self.class.all_nine_letter_words.uniq
+  end
+
 
   def sanitize_all_words
     self.two_letter_words = sanitize(self.two_letter_words)
