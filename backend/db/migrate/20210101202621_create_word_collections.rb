1644d4db311ac86e7a725732ea312f5f3dd62009
David <dmaster18@gmail.com>
2021-01-01 17:08:55 -0500

    Renamed **.word_colelction with **_letter_word_collection

--- backend/db/migrate/20210101202621_create_word_collections.rb
+++ backend/db/migrate/20210101202621_create_word_collections.rb
@@ -2,14 +2,14 @@ class CreateWordCollections < ActiveRecord::Migration[6.0]
   def change
     create_table :word_collections do |t|
       t.string :name
-      t.text :two_word_collection
-      t.text :three_word_collection
-      t.text :four_word_collection
-      t.text :five_word_collection
-      t.text :six_word_collection
-      t.text :seven_word_collection
-      t.text :eight_word_collection
-      t.text :nine_word_collection
+      t.text :two_letter_word_collection
+      t.text :three_letter_word_collection
+      t.text :four_letter_word_collection
+      t.text :five_letter_word_collection
+      t.text :six_letter_word_collection
+      t.text :seven_letter_word_collection
+      t.text :eight_letter_word_collection
+      t.text :nine_letter_word_collection
 
       t.timestamps
     end
