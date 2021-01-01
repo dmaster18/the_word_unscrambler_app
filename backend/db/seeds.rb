e0ea3c5e2698aed1643e86b858a78d97dcc3330d
David <dmaster18@gmail.com>
2021-01-01 15:34:50 -0500

    Add WordCollection instances in seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -3722,6 +3722,19 @@ horatio = Player.new(name: 'Horatio', score: 75)
 esteban = Player.new(name: 'Esteban', score: 550)
 claudia = Player.new(name: 'Claudia', score: 10)
 pablo = Player.new(name: 'Pablo, score: 40')
+
+two_word_collection = WordCollection.new(name: 'Two-Letter Words')
+three_word_collection = WordCollection.new(name: 'Three-Letter Words')
+four_word_collection = WordCollection.new(name: 'Four-Letter Words')
+five_word_collection = WordCollection.new(name: 'Five-Letter Words')
+six_word_collection = WordCollection.new(name: 'Six-Letter Words')
+seven_word_collection = WordCollection.new(name: 'Seven-Letter Words')
+eight_word_collection = WordCollection.new(name: 'Eight-Letter Words')
+nine_word_collection = WordCollection.new(name: 'Nine-Letter Words')
+
+
+
+
 david.save
 raul.save
 nikolai.save
