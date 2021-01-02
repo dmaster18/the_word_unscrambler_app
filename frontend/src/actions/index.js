6d64c8b1a087a302377cff33a0edca27f2fbc746
David <dmaster18@gmail.com>
2021-01-01 19:23:20 -0500

    Updated actions\index.js

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -8,14 +8,19 @@ export function fetchTrainerWords {
     return fetch('http://127.0.0.1:3000/word_collections')
     .then(response => response.json())
     .then(json => dispatch({type: 'FETCH_TRAINER_WORDS_SUCCESS',
-      data: json.data.map(word => ({
-        name: word.attributes.name,
-        letterArray: shuffle(word.attributes.name),
-        allWords: word.attributes.all_words.map((word) => word.toLowerCase())
+      data: json.data.map(word_collection => ({
+        twoLetterWords: word.attributes.two_letter_word_collection,
+        threeLetterWords: word.attributes.three_letter_word_collection,
+        fourLetterWords: word.attributes.four_letter_word_collection,
+        fiveLetterWords: word.attributes.five_letter_word_collection,
+        sixLetterWords: word.attributes.six_letter_word_collection,
+        sevenLetterWords: word.attributes.seven_letter_word_collection,
+        eightLetterWords: word.attributes.eight_letter_word_collection,
+        nineLetterWords: word.attributes.nine_letter_word_collection
       }))
-    }))
-    .catch((error) => {console.log(error); dispatch({type: 'FETCH_WORDS_ERROR'})});
-  };
+  }))
+  .catch((error) => {console.log(error); dispatch({type: 'FETCH_TRAINER_WORDS_ERROR'})});
+};
 }
 
 export function fetchWords(numberOfWords) {
@@ -24,15 +29,16 @@ export function fetchWords(numberOfWords) {
     return fetch(`http://127.0.0.1:3000/words/?limit=${numberOfWords}`)
     .then(response => response.json())
     .then(json => dispatch({type: 'FETCH_WORDS_SUCCESS',
-      data: json.data.map(word => ({
-        name: word.attributes.name,
-        letterArray: shuffle(word.attributes.name),
-        allWords: word.attributes.all_words.map((word) => word.toLowerCase())
-      }))
+    data: json.data.map(word => ({
+      name: word.attributes.name,
+      letterArray: shuffle(word.attributes.name),
+      allWords: word.attributes.all_words.map((word) => word.toLowerCase())
     }))
-    .catch((error) => {console.log(error); dispatch({type: 'FETCH_WORDS_ERROR'})});
-  };
+  }))
+  .catch((error) => {console.log(error); dispatch({type: 'FETCH_WORDS_ERROR'})});
+};
 }
+
 
 export function deleteWord() {
   return {type: 'DELETE_WORD'}
