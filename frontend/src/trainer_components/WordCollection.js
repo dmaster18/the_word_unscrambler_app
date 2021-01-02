b85d0bae5e08112a948038e0c0ddd919df4265aa
David <dmaster18@gmail.com>
2021-01-02 13:52:04 -0500

    Added <div className='word-collections>

--- frontend/src/trainer_components/WordCollection.js
+++ frontend/src/trainer_components/WordCollection.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 const WordCollection = (props) => (
   //<div className= 'wordCollectionColumn'>
-  <div className={props.columnSelector}>
-    <h3>
-      {props.columnHeader}
-    </h3>
-    <ul>
-      {props.columnData.map(word => <li>{word}</li>)}
-    </ul>
+  <div className='word-collections'>
+    <div className={props.columnSelector}>
+      <h3>
+        {props.columnHeader}
+      </h3>
+      <ul>
+        {props.columnData.map(word => <li>{word}</li>)}
+      </ul>
+    </div>
   </div>
 )
 
