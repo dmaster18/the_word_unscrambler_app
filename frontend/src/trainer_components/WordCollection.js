706aa1bad701768a7bf3df26b2908b641c4877d8
David <dmaster18@gmail.com>
2021-01-01 23:44:37 -0500

    Defined WordCollection Component

---
+++ frontend/src/trainer_components/WordCollection.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const WordCollection = (props) => (
+  <div className= 'wordCollectionColumn'>
+    <div className='columnHeader'>{props.columnHeader}</div>
+    <ul className='words'>
+      {props.columnData.map( word => <li>{word}</li>)}
+    </ul>
+  </div>
+)
+
+export default WordCollection
