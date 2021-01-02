5f9a0b8563a229845afd83d2be2d3d78f7c7786d
David <dmaster18@gmail.com>
2021-01-02 15:10:00 -0500

    Changed word_collection backend to remove duplicates

--- backend/app/models/word_collection.rb
+++ backend/app/models/word_collection.rb
@@ -24,14 +24,14 @@ class WordCollection < ApplicationRecord
       self.eight_letter_word_collection << word.eight_letter_words
       self.nine_letter_word_collection << word.nine_letter_words
     }
-    self.two_letter_word_collection = self.two_letter_word_collection.uniq.flatten.sort
-    self.three_letter_word_collection = self.three_letter_word_collection.uniq.flatten.sort
-    self.four_letter_word_collection = self.four_letter_word_collection.uniq.flatten.sort
-    self.five_letter_word_collection = self.five_letter_word_collection.uniq.flatten.sort
-    self.six_letter_word_collection = self.six_letter_word_collection.uniq.flatten.sort
-    self.seven_letter_word_collection = self.seven_letter_word_collection.uniq.flatten.sort
-    self.eight_letter_word_collection = self.eight_letter_word_collection.uniq.flatten.sort
-    self.nine_letter_word_collection = self.nine_letter_word_collection.uniq.flatten.sort
+    self.two_letter_word_collection = self.two_letter_word_collection.flatten.sort.uniq
+    self.three_letter_word_collection = self.three_letter_word_collection.flatten.sort.uniq
+    self.four_letter_word_collection = self.four_letter_word_collection.flatten.sort.uniq
+    self.five_letter_word_collection = self.five_letter_word_collection.flatten.sort.uniq
+    self.six_letter_word_collection = self.six_letter_word_collection.flatten.sort.uniq
+    self.seven_letter_word_collection = self.seven_letter_word_collection.flatten.sort.uniq
+    self.eight_letter_word_collection = self.eight_letter_word_collection.flatten.sort.uniq
+    self.nine_letter_word_collection = self.nine_letter_word_collection.flatten.sort.uniq
     self
   end
 
