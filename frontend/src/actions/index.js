227049c70f025cafcd62d1177cf41e96cbe327f1
David <dmaster18@gmail.com>
2021-01-03 13:09:10 -0500

    Segregate functions by modules

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,26 +1,6 @@
+//Game Functions
 function shuffle(letters) {
   return letters.split('').sort(() => Math.random() - 0.5);
-}
-
-export function fetchTrainerWords() {
-  return (dispatch) => {
-    dispatch({type: 'FETCH_TRAINER_WORDS_START' });
-    return fetch('http://127.0.0.1:3000/word_collections')
-    .then(response => response.json())
-    .then(json => dispatch({type: 'FETCH_TRAINER_WORDS_SUCCESS',
-      data: json.data.map(word_collection => ({
-        twoLetterWords: word_collection.attributes.two_letter_word_collection,
-        threeLetterWords: word_collection.attributes.three_letter_word_collection,
-        fourLetterWords: word_collection.attributes.four_letter_word_collection,
-        fiveLetterWords: word_collection.attributes.five_letter_word_collection,
-        sixLetterWords: word_collection.attributes.six_letter_word_collection,
-        sevenLetterWords: word_collection.attributes.seven_letter_word_collection,
-        eightLetterWords: word_collection.attributes.eight_letter_word_collection,
-        nineLetterWords: word_collection.attributes.nine_letter_word_collection
-      }))
-  }))
-  .catch((error) => {console.log(error); dispatch({type: 'FETCH_TRAINER_WORDS_ERROR'})});
-};
 }
 
 export function fetchWords(numberOfWords) {
@@ -66,6 +46,31 @@ export function startGame() {
 export function endGame() {
   return {type: 'END_GAME'}
 }
+
+//Trainer Functions
+export function fetchTrainerWords() {
+  return (dispatch) => {
+    dispatch({type: 'FETCH_TRAINER_WORDS_START' });
+    return fetch('http://127.0.0.1:3000/word_collections')
+    .then(response => response.json())
+    .then(json => dispatch({type: 'FETCH_TRAINER_WORDS_SUCCESS',
+      data: json.data.map(word_collection => ({
+        twoLetterWords: word_collection.attributes.two_letter_word_collection,
+        threeLetterWords: word_collection.attributes.three_letter_word_collection,
+        fourLetterWords: word_collection.attributes.four_letter_word_collection,
+        fiveLetterWords: word_collection.attributes.five_letter_word_collection,
+        sixLetterWords: word_collection.attributes.six_letter_word_collection,
+        sevenLetterWords: word_collection.attributes.seven_letter_word_collection,
+        eightLetterWords: word_collection.attributes.eight_letter_word_collection,
+        nineLetterWords: word_collection.attributes.nine_letter_word_collection
+      }))
+  }))
+  .catch((error) => {console.log(error); dispatch({type: 'FETCH_TRAINER_WORDS_ERROR'})});
+};
+}
+
+//Leaderboard Functions
+
 
 
 /*
