09bb3381c062ff1747765d5b617f78afa1364e7a
David <dmaster18@gmail.com>
2021-01-03 13:12:18 -0500

    Segregate Reducer Cases by Module

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,15 +1,10 @@
-const initialState = {score: 0, wordIndex: 0, wordSet: [],
+const initialState = {/*Game Variables*/score: 0, wordIndex: 0, wordSet: [],
 gameStatus: 'Loading', usedTiles: [], correctWords: [], incorrectWords: [],
-submittedWords: [], userWarning: null, trainerWords: [], trainerStatus: 'Loading', submitStatus: 'Loading', playerData: {}}
+submittedWords: [], userWarning: null, /*Trainer Variables*/ trainerWords: [], trainerStatus: 'Loading', /*Leaderboard Variables*/submitStatus: 'Loading', playerData: {}}
 
 export default function reducer(state=initialState, action) {
   switch(action.type) {
-    case 'FETCH_TRAINER_WORDS_START':
-      return initialState
-    case 'FETCH_TRAINER_WORDS_SUCCESS':
-      return {...state, trainerWords: action.data, trainerStatus: 'Running'}
-    case 'FETCH_TRAINER_WORDS_ERROR':
-      return {...state, trainerWords: [], trainerStatus: 'Error'}
+    //Game Cases
     case 'FETCH_WORDS_START':
       return initialState
     case 'FETCH_WORDS_SUCCESS':
@@ -63,6 +58,14 @@ export default function reducer(state=initialState, action) {
       return {...state, gameStatus: 'Running'}
     case 'END_GAME':
       return {...state, gameStatus: 'Complete'}
+    //Trainer Cases
+    case 'FETCH_TRAINER_WORDS_START':
+      return initialState
+    case 'FETCH_TRAINER_WORDS_SUCCESS':
+      return {...state, trainerWords: action.data, trainerStatus: 'Running'}
+    case 'FETCH_TRAINER_WORDS_ERROR':
+      return {...state, trainerWords: [], trainerStatus: 'Error'}
+    //Leaderboard Cases
     case 'SEND_PLAYER_DATA_START':
       return initialState
     case 'SEND_PLAYER_DATA_SUCCESS':
